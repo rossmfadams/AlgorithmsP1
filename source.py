1b38901b81f46c7b3cca796b7e214fd4820ef067
Longtin Hang <h.longtin@yahoo.com>
2020-02-01 19:35:18 -0600

    Finding p and q

--- source.py
+++ source.py
@@ -1,1 +1,34 @@
-## Place your code here
+# Place your code here
+import math
+import random
+
+
+def test_prime_brute_force(p=10):
+    if p == 2:
+        return True
+    else:
+        for b in range(2, math.floor(math.sqrt(p))):
+            if math.gcd(p, b) > 1:
+                return False
+            else:
+                continue
+        return True
+
+
+def generating_prime_numbers():
+    my_prime_list = []
+
+    for i in range(100):
+        value = random.randint(10000, 20000)
+        if test_prime_brute_force(value):
+            my_prime_list.append(value)
+
+    return my_prime_list
+
+
+def main():
+    prime = generating_prime_numbers()
+    print(prime)
+
+
+main()
