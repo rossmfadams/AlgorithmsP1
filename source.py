c5f24576a5b3547b5d5a7a0f5320140946836e2e
Ross Adams <radams9@cub.uca.edu>
2020-02-03 14:56:18 -0600

    Implemented the extended_gcd algorithm and a method for generating both the Public and Private keys. Untested

--- source.py
+++ source.py
@@ -30,5 +30,33 @@ def main():
     prime = generating_prime_numbers()
     print(prime)
 
+def extended_gcd(a=1,b=1):
+    if a < b:
+        a, b = b, a
+        print('Switched two arguments to ensure a >= b.')
+    if b == 0:
+        return (1,0,a)
+    (x,y,d) = extended_gcd(b,a%b)
+    return y,x - a//b*y, d
 
-main()
+def generate_keys():
+    
+    ## Public key
+    primes = generating_prime_numbers()
+    while True:
+        p = primes[random.randint(0,100)]
+        q = primes[random.randint(0,100)]
+        if gcd(p,q) == 1:
+            break
+    n = p * q
+    fn = (p-1)(q-1)
+
+    while True:
+        e = random.randint(2,fn)
+        if gcd(fn,e) == 1:
+            break
+    ## Private key
+    (x,y,d) = extended_gcd(fn,e)
+
+    return (n, e, d)
+
